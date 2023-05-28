56a05b443f2d78df590190d3aefe45e1998c9c2c
NnaemekaAlgorithim <nnaemekanwodo625@gmail.com>
2023-05-28 18:07:07 +0100

    fixing bugs

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,13 @@ import os
 """creating an instance of the class Flask and assign it to the variable app"""
 app = Flask(__name__)
 
+# global strict slashes
+app.url_map.strict_slashes = False
+
+# flask server environmental setup
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
+
 """Register the app_views blueprint"""
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
@@ -18,7 +25,10 @@ def teardown_appcontext(exception):
     storage.close()
 
 if __name__ == "__main__":
-    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.environ.get('HBNB_API_PORT', 5000))
-
-    app.run(host=host, port=port, threaded=True)
+    """
+    MAIN Flask App
+    """
+    # initializes global error handling
+    setup_global_errors()
+    # start Flask app
+    app.run(host=host, port=port)
